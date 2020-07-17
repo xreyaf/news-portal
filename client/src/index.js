c656b3f2b1988b7ddf247692ecf810581604dc79
Asad <baxodirovich777@gmail.com>
2020-07-17 16:34:21 +0300

    heroku

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const port = process.env.PORT || 3001;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
